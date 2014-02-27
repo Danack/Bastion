e05cfef71e6d6532794b2a0b1de73c75ef7a0084
Danack <Danack@basereality.com>
2014-02-27 20:58:44 +0000

    Fixing paths.

--- copy-below-root/config.php
+++ copy-below-root/config.php
@@ -9,3 +9,7 @@ $accessToken = false;
 define('AWS_SERVICES_KEY', '12345');
 define('AWS_SERVICES_SECRET', '12345');
 
+
+$ignoreList = "./ignoreList.txt";
+$usingList = "./usingList.txt";
+$outputDirectory = "./zipsOutput/packages";
--- runArtifact.sh
+++ runArtifact.sh
@@ -2,17 +2,15 @@
 
 set -e
 
-php getArtifacts.php
+php ./src/getArtifacts.php
 
 #why the heck does this even attempt to run scripts?
 php vendor/bin/satis build satis-zips.json zipsOutput -vv
  
 # --no-scripts
 
-php fixPaths.php
+php ./src/fixPaths.php
 
 # php syncArtifactBuild.php
-
 # php -S localhost:8000 -t zipsOutput/
-
 # php -S localhost:80 -t zipsOutput/
